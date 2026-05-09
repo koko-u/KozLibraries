9b30ac3b950a9d622296a5acd9ae4c309a29e352
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-05-09 16:15:20 +0900

    Create Attribute for Services

---
+++ src/AutoRegisterAnnotation/AutoRegisterServiceAttribute.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace AutoRegisterAnnotation;
+
+/// <summary>
+/// Annotation for classes that should be automatically registered as services.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, Inherited = true)]
+[MeansImplicitUse(
+    ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature,
+    ImplicitUseTargetFlags.Itself
+)]
+public sealed class AutoRegisterServiceAttribute : Attribute;

0b1743c6321ae8f983c36a2298c8f6cd2e3f20df
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-05-10 17:09:05 +0900

    add debug log

--- src/TagHelpers/BsValidationClassTagHelper.cs
+++ src/TagHelpers/BsValidationClassTagHelper.cs
@@ -1,14 +1,17 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
 
 namespace KozLibraries.TagHelpers;
 
 [HtmlTargetElement("input", Attributes = "asp-for, bs-valid")]
 [HtmlTargetElement("textarea", Attributes = "asp-for, bs-valid")]
 [HtmlTargetElement("select", Attributes = "asp-for, bs-valid")]
-public sealed class BsValidationClassTagHelper : TagHelper
+public sealed class BsValidationClassTagHelper(ILogger<BsValidationClassTagHelper> logger)
+    : TagHelper
 {
     [ViewContext]
     [HtmlAttributeNotBound]
@@ -24,6 +27,8 @@ public sealed class BsValidationClassTagHelper : TagHelper
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
+        logger.LogDebug("bs-valid TagHelper invoked.");
+
         // bypass hidden inputs
         if (output.IsHiddenInput())
         {
@@ -32,19 +37,26 @@ public sealed class BsValidationClassTagHelper : TagHelper
 
         // asp-for attribute name
         var fullName = ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(For.Name);
+        logger.LogDebug($"Processing bs-valid for field: {fullName}");
 
         // ModelState error of attribute name
         if (!ViewContext.ViewData.ModelState.TryGetValue(fullName, out var modelStateEntry))
         {
+            logger.LogDebug($"No model state entry found for field: {fullName}");
             return;
         }
 
+        logger.LogDebug($"{fullName}'s validation state: {modelStateEntry.ValidationState}");
+        logger.LogDebug(
+            $"{fullName}'s validation error: {modelStateEntry.Errors.FirstOrDefault()?.ErrorMessage}"
+        );
         if (
             modelStateEntry.ValidationState == ModelValidationState.Invalid
             || modelStateEntry.Errors.Count > 0
         )
         {
             output.AddCssClass("is-invalid");
+            output.Attributes.SetAttribute("aria-describedby", $"{fullName}_Feedback");
         }
         else
         {
--- src/TagHelpers/BsValidationMessageTagHelper.cs
+++ src/TagHelpers/BsValidationMessageTagHelper.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
 
 namespace KozLibraries.TagHelpers;
 
 [HtmlTargetElement(Attributes = "asp-validation-for, bs-feedback")]
-public sealed class BsValidationMessageTagHelper : TagHelper
+public sealed class BsValidationMessageTagHelper(ILogger<BsValidationMessageTagHelper> logger)
+    : TagHelper
 {
     [ViewContext]
     [HtmlAttributeNotBound]
@@ -21,6 +23,13 @@ public sealed class BsValidationMessageTagHelper : TagHelper
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
+        logger.LogDebug("bs-feedback TagHelper invoked.");
+
+        // asp-for attribute name
+        var fullName = ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(For.Name);
+        logger.LogDebug($"Processing bs-valid for field: {fullName}");
+
+        output.Attributes.SetAttribute("id", $"{fullName}_Feedback");
         output.AddCssClass("invalid-feedback");
     }
 }

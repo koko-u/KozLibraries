b540e93cb02a8ad0457e85637e2ad0a921f2aa39
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-05-12 04:53:53 +0900

    add Dapper DateOnly Support

---
+++ src/DapperDateOnlySupport/DateOnlyHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace KozLibraries.DapperDateOnlySupport;
+
+public sealed class DateOnlyHandler : SqlMapper.TypeHandler<DateOnly>
+{
+    /// <summary>
+    /// set DateOnly value to parameter as DateTime
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <param name="value"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public override void SetValue(IDbDataParameter parameter, DateOnly value)
+    {
+        parameter.Value = value.ToDateTime(TimeOnly.MinValue);
+    }
+
+    /// <summary>
+    /// parse query result to DateOnly
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public override DateOnly Parse(object value)
+    {
+        var datetime = (DateTime)value;
+        return DateOnly.FromDateTime(datetime);
+    }
+}

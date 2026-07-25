63f98fc403b23fdac64dff49b8f94d653098827d
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-07-25 16:01:41 +0900

    implicit TxSession into ConnSession

--- src/TransactionRunner/Data/ConnSession.cs
+++ src/TransactionRunner/Data/ConnSession.cs
@@ -2,4 +2,7 @@ using Npgsql;
 
 namespace KozLibraries.TransactionRunner.Data;
 
-public readonly record struct ConnSession(NpgsqlConnection Connection);
+public readonly record struct ConnSession(NpgsqlConnection Connection)
+{
+    public static implicit operator ConnSession(TxSession session) => new(session.Connection);
+}
